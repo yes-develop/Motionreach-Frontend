c38c6aeb0b93c3b58a45c07bd6116a4c66bf8e76
thanakorn <thanakorn.tp10@gmail.com>
2026-05-13 14:19:29 +0700

    Revert "Initial commit: sync from Motionreach-front"
    This reverts commit 552f395a4ac80b0b58ac8b38d8e63d93f8165b59.

--- android/app/src/main/kotlin/com/example/MotionReach/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.MotionReach
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/data/notifires.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-ValueNotifier<int> selectedPageNotifier = ValueNotifier(0);
